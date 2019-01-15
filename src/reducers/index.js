f258975edef439b38b5425329158de02a144a670
Singhtaravirak Chea <singhtaravirak@gmail.com>
2019-01-15 08:42:12 +0700

    building reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,15 @@
+export const songsReducer = () => {
+  return [
+    { title: 'He lives in you', duration: '4:51' },
+    { title: 'Circle of life', duration: '3:50' },
+    { title: 'Fix you', duration: '4:10' },
+    { title: 'Scientist', duration: '4:00' }
+  ];
+};
 
+export const selectedSongReducer = (selectedSong = null, action) => {
+  if(action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+  return selectedSong;
+};
