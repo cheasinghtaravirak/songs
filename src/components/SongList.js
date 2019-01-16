8b93b7bba449506869decc21d44ebf97ab558731
Singhtaravirak Chea <singhtaravirak@gmail.com>
2019-01-16 09:59:56 +0700

    update our application state (or redux store) by calling an action creator (selectSong) on our <SongList /> whenever click a button

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends Component {
 
@@ -9,7 +10,10 @@ class SongList extends Component {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
               Select
             </button>
           </div>
@@ -23,7 +27,7 @@ class SongList extends Component {
 
 
   render() {
-    console.log(this.props);
+    console.log(this.props); 
     return (
       <div className="ui divided list">{this.renderList()}</div>
     );
@@ -31,9 +35,10 @@ class SongList extends Component {
 }
 
 const mapStateToProps = (state) => {
+  console.log(state);
   return {
     songs: state.songs,
   };
 }
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
